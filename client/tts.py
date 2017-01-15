3d95a5f718b6a716ff0cb8fe22fbe3c9330f3089
Dave Benvenuti <davebenvenuti@gmail.com>
2017-01-15 13:01:25 -0800

    use aplay again with bluealsa device

--- client/tts.py
+++ client/tts.py
@@ -73,7 +73,7 @@ class AbstractTTSEngine(object):
     def play(self, filename):
         # FIXME: Use platform-independent audio-output here
         # See issue jasperproject/jasper-client#188
-        cmd = ['paplay', str(filename)]
+        cmd = ['aplay', '-D', 'bluealsa', str(filename)]
         self._logger.debug('Executing %s', ' '.join([pipes.quote(arg)
                                                      for arg in cmd]))
         with tempfile.TemporaryFile() as f:
